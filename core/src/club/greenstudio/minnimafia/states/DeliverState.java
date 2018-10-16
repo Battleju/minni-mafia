bd268884f779c67a941c3f9196323cce0953af62
Battleju <godanbo@googlemail.com>
2018-10-16 21:20:32 +0200

    alpha 0.1_1

---
+++ core/src/club/greenstudio/minnimafia/states/DeliverState.java
@@ -0,0 +1,30 @@
+package club.greenstudio.minnimafia.states;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class DeliverState extends State {
+
+    public DeliverState(GameStateManager gsm, State lastState) {
+        super(gsm, lastState);
+    }
+
+    @Override
+    protected void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
